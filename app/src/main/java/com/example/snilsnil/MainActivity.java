ae67c9b304c9c530dfcca2813837849afe274269
snilsnil <heo6601@naver.com>
2023-03-14 12:31:05 +0900

    add for button

--- app/src/main/java/com/example/snilsnil/MainActivity.java
+++ app/src/main/java/com/example/snilsnil/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.snilsnil;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,10 +18,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         
-        /* 2주차
-        *  버튼 만들기
-        * */
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        /* 2주차 */
+
+        /* 버튼 만들기 */
+        /*getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.drawable.q);
 
         Button button1 = findViewById(R.id.button1);
@@ -34,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         
-        /* 체크박스 만들기 */
+        *//* 체크박스 만들기 *//*
         CheckBox check1;
 
         check1 = (CheckBox) findViewById(R.id.check1);
@@ -45,6 +47,47 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "체크박스 클릭",
                         Toast.LENGTH_SHORT).show();
+            }
+        });*/
+
+
+
+        /* 4개 버튼 */
+        Button btnNate, btn911, btnGal, btnEnd;
+
+        btnNate = findViewById(R.id.btnNate);
+        btn911 = findViewById(R.id.btn911);
+        btnGal = findViewById(R.id.btnGal);
+        btnEnd = findViewById(R.id.btnEnd);
+
+        btnNate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.nate.com"));
+                startActivity(mIntent);
+            }
+        });
+
+        btn911.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:/911"));
+                startActivity(mIntent);
+            }
+        });
+
+        btnGal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media"));
+                startActivity(mIntent);
+            }
+        });
+
+        btnEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
