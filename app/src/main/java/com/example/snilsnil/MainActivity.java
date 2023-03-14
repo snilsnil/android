d50b622f81977c696a18d4f9206d27ef65cd8b3e
snilsnil <heo6601@naver.com>
2023-03-14 11:03:20 +0900

    add Button

--- app/src/main/java/com/example/snilsnil/MainActivity.java
+++ app/src/main/java/com/example/snilsnil/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.snilsnil;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
+        /* 2주차
+        *  버튼 만들기
+        * */
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.q);
+
+        Button button1 = findViewById(R.id.button1);
+        
+        
+        //버튼을 클릭했을때, 이벤트 발생
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Toast.makeText(MainActivity.this, "버튼을 눌렀어요"
+                , Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        
     }
 }
